816c2a943eea90f2e072af26722a643eae293b84
vaddekrishn <krishnaxkv@gmail.com>
2024-05-31 23:30:25 -0600

    Save this as 93% doen got till here we are able to achive all the core futures working on extra function that need to take care

--- background.js
+++ background.js
@@ -42,91 +42,101 @@ let copiedData = {
   }
   
   createContextMenus();
-
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
-  const restrictedUrls = ['chrome://', 'https://chrome.google.com/webstore'];
-  const isRestrictedUrl = restrictedUrls.some(url => tab.url.startsWith(url));
-
-  if (isRestrictedUrl) {
-    console.warn(`Cannot copy or paste data on restricted URL: ${tab.url}`);
-    return;
-  }
-
-  if (info.menuItemId === "copy-text1") {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: copyData,
-      args: ["text1", info]
-    });
-  } else if (info.menuItemId === "copy-text2") {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: copyData,
-      args: ["text2", info]
-    });
-  } else if (info.menuItemId === "paste-text1") {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: pasteData,
-      args: [copiedData.text1]
-    });
-  } else if (info.menuItemId === "paste-text2") {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: pasteData,
-      args: [copiedData.text2]
-    });
-  }
-});
-
-function copyData(type, info) {
-  let copiedContent;
-
-  if (info.selectionText) {
-    copiedContent = info.selectionText;
-    console.log(`Text copied (${type}):`, copiedContent);
-  } else if (info.linkUrl) {
-    copiedContent = info.linkUrl;
-    console.log(`Link copied (${type}):`, copiedContent);
-  } else if (info.srcUrl) {
-    copiedContent = info.srcUrl;
-    console.log(`Image copied (${type}):`, copiedContent);
-  } else {
-    const targetElement = info.targetElementObject;
-    if (targetElement) {
-      copiedContent = targetElement.outerHTML;
-      console.log(`HTML element copied (${type}):`, copiedContent);
-    } else {
-      console.warn(`No data selected for ${type}`);
+  
+  chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    const restrictedUrls = ['chrome://', 'https://chrome.google.com/webstore'];
+    const isRestrictedUrl = restrictedUrls.some(url => tab.url.startsWith(url));
+  
+    if (isRestrictedUrl) {
+      console.warn(`Cannot copy or paste data on restricted URL: ${tab.url}`);
       return;
     }
-  }
-
-  chrome.runtime.sendMessage({ action: "storeCopiedData", type: type, data: copiedContent });
-}
-
-function pasteData(data) {
-  const activeElement = document.activeElement;
-  if (activeElement) {
-    if (activeElement.isContentEditable) {
-      document.execCommand('insertText', false, data);
-      console.log(`Data pasted into contentEditable element.`);
-    } else if (activeElement.tagName === 'TEXTAREA' || activeElement.tagName === 'INPUT') {
-      activeElement.value = data;
-      console.log(`Data pasted into textarea or input element.`);
-    } else {
-      console.warn(`Unsupported element type (${activeElement.tagName}) for pasting data.`);
+  
+    if (info.menuItemId === "copy-text1") {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: copyData,
+        args: ["text1", info]
+      });
+    } else if (info.menuItemId === "copy-text2") {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: copyData,
+        args: ["text2", info]
+      });
+    } else if (info.menuItemId === "paste-text1") {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: pasteData,
+        args: [copiedData.text1]
+      });
+    } else if (info.menuItemId === "paste-text2") {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: pasteData,
+        args: [copiedData.text2]
+      });
     }
-  } else {
-    console.warn(`No active element found to paste data into.`);
+  });
+  
+  function copyData(type, info) {
+    let copiedContent;
+  
+    if (info.selectionText) {
+      copiedContent = info.selectionText;
+      console.log(`Text copied (${type}):`, copiedContent);
+    } else if (info.linkUrl) {
+      copiedContent = info.linkUrl;
+      console.log(`Link copied (${type}):`, copiedContent);
+    } else if (info.srcUrl) {
+      copiedContent = info.srcUrl;
+      console.log(`Image copied (${type}):`, copiedContent);
+    } else {
+      const targetElement = info.targetElementObject;
+      if (targetElement) {
+        copiedContent = targetElement.outerHTML;
+        console.log(`HTML element copied (${type}):`, copiedContent);
+      } else {
+        console.warn(`No data selected for ${type}`);
+        return;
+      }
+    }
+  
+    chrome.runtime.sendMessage({ action: "storeCopiedData", type: type, data: copiedContent });
   }
-}
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action === "storeCopiedData") {
-    copiedData[request.type] = request.data;
+  
+  function pasteData(data) {
+    const activeElement = document.activeElement;
+    if (activeElement) {
+      if (activeElement.isContentEditable) {
+        const selection = window.getSelection();
+        const range = selection.getRangeAt(0);
+        const newNode = document.createTextNode(data);
+        range.insertNode(newNode);
+        range.setStartAfter(newNode);
+        selection.removeAllRanges();
+        selection.addRange(range);
+        console.log(`Data pasted into contentEditable element.`);
+      } else if (activeElement.tagName === 'TEXTAREA' || activeElement.tagName === 'INPUT') {
+        const start = activeElement.selectionStart;
+        const end = activeElement.selectionEnd;
+        const value = activeElement.value;
+        activeElement.value = value.slice(0, start) + data + value.slice(end);
+        activeElement.setSelectionRange(start + data.length, start + data.length);
+        console.log(`Data pasted into textarea or input element.`);
+      } else {
+        console.warn(`Unsupported element type (${activeElement.tagName}) for pasting data.`);
+      }
+    } else {
+      console.warn(`No active element found to paste data into.`);
+    }
   }
-});
-
-chrome.runtime.onInstalled.addListener(createContextMenus);
-chrome.runtime.onStartup.addListener(createContextMenus);
+  
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === "storeCopiedData") {
+      copiedData[request.type] = request.data;
+    }
+  });
+  
+  chrome.runtime.onInstalled.addListener(createContextMenus);
+  chrome.runtime.onStartup.addListener(createContextMenus);
